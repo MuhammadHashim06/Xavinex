323f133c4a8509dd5134e5f10a4f5a483b5168e3
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-12-14 10:10:53 +0500

    Updated Service Page

--- Portfolio.client/src/components/ServicesPage/components/HeroSection.jsx
+++ Portfolio.client/src/components/ServicesPage/components/HeroSection.jsx
@@ -6,6 +6,7 @@ import {
   advancedTechSolutions,
   generativeAI,
 } from "../data/ServiceData";
+import consultation from '../../../assets/Challaenges.png'
 import { NavLink } from "react-router-dom";
 
 export default function HeroSection() {
@@ -33,10 +34,10 @@ export default function HeroSection() {
           <ul>
             {aiSolutions.map((s, index) => {
               return (
-                <NavLink to={''} key={index}>
-                  <li >{s.head}</li>
+                <NavLink to={""} key={index}>
+                  <li>{s.head}</li>
                 </NavLink>
-              )
+              );
             })}
           </ul>
         </div>
@@ -44,9 +45,9 @@ export default function HeroSection() {
           <h1>Product Development</h1>
           <ul>
             {productDevelopment.map((s, index) => {
-               return (
-                <NavLink to={''} key={index}>
-                  <li >{s.head}</li>
+              return (
+                <NavLink to={""} key={index}>
+                  <li>{s.head}</li>
                 </NavLink>
               );
             })}
@@ -56,9 +57,9 @@ export default function HeroSection() {
           <h1>Business Advisory</h1>
           <ul>
             {businessAdvisory.map((s, index) => {
-               return (
-                <NavLink to={''} key={index}>
-                  <li >{s.head}</li>
+              return (
+                <NavLink to={""} key={index}>
+                  <li>{s.head}</li>
                 </NavLink>
               );
             })}
@@ -68,9 +69,9 @@ export default function HeroSection() {
           <h1>Advance Tech Solution</h1>
           <ul>
             {advancedTechSolutions.map((s, index) => {
-               return (
-                <NavLink to={''} key={index}>
-                  <li >{s.head}</li>
+              return (
+                <NavLink to={""} key={index}>
+                  <li>{s.head}</li>
                 </NavLink>
               );
             })}
@@ -80,9 +81,9 @@ export default function HeroSection() {
           <h1>Generative AI</h1>
           <ul>
             {generativeAI.map((s, index) => {
-               return (
-                <NavLink to={''} key={index}>
-                  <li >{s.head}</li>
+              return (
+                <NavLink to={""} key={index}>
+                  <li>{s.head}</li>
                 </NavLink>
               );
             })}
@@ -91,7 +92,26 @@ export default function HeroSection() {
         <div className="servicecard help">
           <h1>Ready to get started?</h1>
           <p>Business Excellence Starts Here: Unlock Our Services.</p>
-          <button>Let s chat  <i className="fas fa-arrow-right" ></i> </button>
+          <button>
+            Let s chat <i className="fas fa-arrow-right"></i>{" "}
+          </button>
+        </div>
+      </div>
+      <div className="bookconsultation">
+        <div className="leftpanel">
+          <h1>
+            So we built the offshore tech staffing firm we always wished we had.
+          </h1>
+          <p>
+            Xavinex mission is to make finding, hiring and paying offshore
+            developers super painless and hyper transparent.Most importantly, we
+            get you amazing developers without forcing you to lose control of
+            the team or the product development process.
+          </p>
+          <button>Book a consultation</button>
+        </div>
+        <div className="rightpanel">
+            <img src={consultation} alt='' />
         </div>
       </div>
     </div>
