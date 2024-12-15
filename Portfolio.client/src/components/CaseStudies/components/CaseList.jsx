b619e4569a818d4a85a855f7cbdd28e3cca5ebf9
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-12-15 15:38:06 +0500

    Updated case studies

--- Portfolio.client/src/components/CaseStudies/components/CaseList.jsx
+++ Portfolio.client/src/components/CaseStudies/components/CaseList.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { caseStudies } from "../data/CaseStudies";
 import "./CaseList.css";
 export default function CaseList() {
   return (
@@ -12,7 +14,44 @@ export default function CaseList() {
         </p>
       </div>
       <div className="list">
-        
+        {caseStudies.map((cs, index) => {
+          return (
+            <div
+              className="caseCard"
+              key={index}
+              style={{ backgroundColor: cs.background,color:cs.color }}
+            >
+              <div className="leftpanel">
+                <div className="logo">
+                  <img src={cs.logo} alt="" />
+                </div>
+                <div className="middletext">
+                  <h1>{cs.title}</h1>
+                  <p>{cs.description}</p>
+                  <div className="techstack">
+                    {cs.stack && cs.stack.length > 0 ? (
+                      cs.stack.map((s, idx) => {
+                        return <li key={idx}>{s}</li>;
+                      })
+                    ) : (
+                      <p>No tech stack available</p>
+                    )}
+                  </div>
+                  <Link to={cs.link || "#"}>
+                    <button>
+                      Explore <i className="fas fa-arrow-right"></i>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+              <div className="rightpanel">
+                <div className="image">
+                  <img src={cs.image || "/default-image.jpg"} alt="" />{" "}
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
